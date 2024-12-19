fd0d89010c3953711ee29aaf73a1b3d2f62ddc10
Dushyant <bobsingh149@gmail.com>
2024-12-19 19:01:59 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const launchUrl = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
+  const LinkCard = ({ title, url, icon }) => (
+    <div className="card" onClick={() => launchUrl(url)}>
+      <div className="card-content">
+        <FontAwesomeIcon icon={icon} className="card-icon" />
+        <span className="card-title">{title}</span>
+        <FontAwesomeIcon icon={faArrowRight} className="card-arrow" />
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>Project Links</h1>
       </header>
+      <div className="container">
+        <div className="content">
+          <h1 className="title">Cliffhanger</h1>
+          <p className="subtitle">Book Social Media</p>
+          
+          <div className="links-container">
+            <LinkCard 
+              title="Frontend Repository"
+              url="https://github.com/ArtFoliers/barter-frontend"
+              icon={faGithub}
+            />
+            <LinkCard 
+              title="Backend Repository"
+              url="https://github.com/ArtFoliers/Barter-backend"
+              icon={faGithub}
+            />
+            <LinkCard 
+              title="Website"
+              url="https://www.cliffhanger.in"
+              icon={faGlobe}
+            />
+            <LinkCard 
+              title="Android App"
+              url="https://drive.google.com/file/d/1Sq32kk-FeASRwzpmBtGiDAnj_x6KZPGL/view?usp=sharing"
+              icon={faGooglePlay}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
